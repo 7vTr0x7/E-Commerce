01c67ce145028cee611a4de5467a9eed0ba4f533
7vTr0x7 <7vtrox7@gmail.com>
2024-04-01 18:19:43 +0530

    Cart

--- front-end/src/pages/cart.tsx
+++ front-end/src/pages/cart.tsx
@@ -1,8 +1,20 @@
-import { set } from "firebase/database";
 import { useEffect, useState } from "react";
 import { VscError } from "react-icons/vsc";
+import CartItem from "../components/CartItem";
+import { Link } from "react-router-dom";
 
-const cartItems = [];
+const cartItems = [
+  {
+    productId: "jbkjb",
+    name: "iPhone",
+    price: 999,
+    stock: 5,
+    quantity: 3,
+    photo:
+      "https://www.apple.com/newsroom/images/2023/09/apple-debuts-iphone-15-and-iphone-15-plus/article/Apple-iPhone-15-lineup-hero-geo-230912_inline.jpg.large.jpg",
+    handler: () => {},
+  },
+];
 const subTotal = 999;
 const shippingCharges = 200;
 const tax = Math.round(subTotal * 0.18);
@@ -27,7 +39,15 @@ const Cart = () => {
 
   return (
     <div className="cart">
-      <main></main>
+      <main>
+        {cartItems.length > 0 ? (
+          cartItems.map((item, index) => (
+            <CartItem key={index} cartItem={item} />
+          ))
+        ) : (
+          <h1>No Items Added</h1>
+        )}
+      </main>
       <aside>
         <p>Sub Total : ₹{subTotal}</p>
         <p>Shipping Charges : ₹{shippingCharges}</p>
@@ -56,6 +76,8 @@ const Cart = () => {
               Invalid Coupon <VscError />{" "}
             </span>
           ))}
+
+        {cartItems.length > 0 && <Link to={"/shipping"}>CheckOut</Link>}
       </aside>
     </div>
   );
