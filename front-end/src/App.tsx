96a9fe88aff1b4b5159ccd52a13a5a9d614f61a8
7vTr0x7 <7vtrox7@gmail.com>
2024-04-01 15:26:45 +0530

    router

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import Search from "./pages/search";
+import Cart from "./pages/cart";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
