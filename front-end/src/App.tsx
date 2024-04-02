9934fb488b179b557d9ae626930a0591891d33e4
7vTr0x7 <7vtrox7@gmail.com>
2024-04-02 15:05:06 +0530

    Login

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -8,6 +8,7 @@ const Home = lazy(() => import("./pages/home"));
 const Search = lazy(() => import("./pages/search"));
 const Cart = lazy(() => import("./pages/cart"));
 const Shipping = lazy(() => import("./pages/shipping"));
+const Login = lazy(() => import("./pages/login"));
 
 const Dashboard = lazy(() => import("./pages/admin/dashboard"));
 const Products = lazy(() => import("./pages/admin/products"));
@@ -37,6 +38,8 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/search" element={<Search />} />
           <Route path="/cart" element={<Cart />} />
+
+          <Route path="/login" element={<Login />} />
 
           <Route>
             <Route path="/shipping" element={<Shipping />} />
