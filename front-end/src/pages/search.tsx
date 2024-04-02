3acda7dec237b5278cad87e3c060b03c3e08dad5
7vTr0x7 <7vtrox7@gmail.com>
2024-04-02 15:35:48 +0530

    search

--- front-end/src/pages/search.tsx
+++ front-end/src/pages/search.tsx
@@ -1,5 +1,91 @@
+import { useState } from "react";
+import ProductCard from "../components/ProductCard";
+
 const Search = () => {
-  return <div>Search</div>;
+  const [search, setSearch] = useState("");
+  const [sort, setSort] = useState("");
+  const [maxPrice, setMaxPrice] = useState(100000);
+  const [category, setCategory] = useState("");
+  const [page, setPage] = useState(1);
+
+  const addToCartHandler = () => {};
+
+  const isPrev = page > 1;
+  const isNext = page < 4;
+
+  return (
+    <div className="product-search-page">
+      <aside>
+        <h2>Filters</h2>
+        <div>
+          <h4>Sort</h4>
+          <select value={sort} onChange={(e) => setSort(e.target.value)}>
+            <option value="">None</option>
+            <option value="asc">Price (Low To High)</option>
+            <option value="dsc">Price (High To Low)</option>
+          </select>
+        </div>
+        <div>
+          <h4>Max Price: {maxPrice || ""}</h4>
+          <input
+            type="range"
+            min={100}
+            max={100000}
+            value={maxPrice}
+            onChange={(e) => setMaxPrice(Number(e.target.value))}
+          />
+        </div>
+
+        <div>
+          <h4>Category</h4>
+          <select
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}>
+            <option value="">All</option>
+            <option value="">Camera</option>
+            <option value="">Game</option>
+            <option value="">Laptop</option>
+          </select>
+        </div>
+      </aside>
+      <main>
+        <h1>Products</h1>
+        <input
+          type="text"
+          placeholder="Search By Name. . ."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+
+        <div className="search-product-list">
+          <ProductCard
+            productId="jbkjb"
+            name="iPhone"
+            price={999}
+            stock={5}
+            photo="https://www.apple.com/newsroom/images/2023/09/apple-debuts-iphone-15-and-iphone-15-plus/article/Apple-iPhone-15-lineup-hero-geo-230912_inline.jpg.large.jpg"
+            handler={addToCartHandler}
+          />
+        </div>
+
+        <article>
+          <button
+            disabled={!isPrev}
+            onClick={() => setPage((prev) => prev - 1)}>
+            Prev
+          </button>
+          <span>
+            {page} of {4}
+          </span>
+          <button
+            disabled={!isNext}
+            onClick={() => setPage((prev) => prev + 1)}>
+            Next
+          </button>
+        </article>
+      </main>
+    </div>
+  );
 };
 
 export default Search;
