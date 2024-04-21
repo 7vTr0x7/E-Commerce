663b222f16753a15a80f8c61aa05b8f9c030f830
7vTr0x7 <7vtrox7@gmail.com>
2024-04-21 19:55:44 +0530

    user route

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,8 +1,14 @@
 import express from "express";
 
+//routes
+import userRoute from "./routes/user.js";
+
 const port: number = 4000;
 
 const app = express();
+
+//using routes
+app.use("/api.v1/user", userRoute);
 
 app.listen(port, () => {
   console.log(`server running on http://localhost:${port}`);
